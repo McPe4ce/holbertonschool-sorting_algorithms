06bcabc41fc376d5b67e06a0abf22953bf6005f3
Mcpe4ce <phil.ghanem@gmail.com>
2026-04-07 15:09:43 +0200

    Implement selection sort algorithm and add function documentation

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array using the selection style
+ * Meaning it will scan the array to find the smallest value
+ * If that value isnt at array[0], swap once
+ * Restart at pos1, and repeat until it is sorted
+ * @array: Array that stores the values
+ * @size: Size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t index;
+	size_t lil_index;
+	size_t dedex;
+	int temp;
+
+	for (index = 0; index < size - 1; index++)
+	{
+		lil_index = index;
+
+		for (dedex = index + 1; dedex < size; dedex++)
+		{
+			if (array[dedex] < array[lil_index])
+			{
+				temp = array[index];
+				array[index] = array[lil_index];
+				array[lil_index] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
