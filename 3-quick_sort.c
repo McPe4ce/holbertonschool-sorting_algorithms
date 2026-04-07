8a9f5b87dc4838e62bc937c7693194eebce3fe11
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-04-07 16:05:39 +0200

    3-O and 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,12 +8,12 @@
 
 void swap_int(int *a, int *b)
 {
-    if (*a != *b)
-    {
-        int tmp = *a;
-        *a = *b;
-        *b = tmp;
-    }
+	if (*a != *b)
+	{
+		int tmp = *a;
+		*a = *b;
+		*b = tmp;
+	}
 }
 
 /**
@@ -27,28 +27,28 @@ void swap_int(int *a, int *b)
 
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low - 1;
-    int j;
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
 
-    for (j = low; j < high; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                swap_int(&array[i], &array[j]);
-                print_array(array, size);
-            }
-        }
-    }
-    if (i + 1 != high)
-    {
-        swap_int(&array[i + 1], &array[high]);
-        print_array(array, size);
-    }
-    return (i + 1);
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap_int(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (i + 1 != high)
+	{
+		swap_int(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
 }
 
 /**
@@ -61,13 +61,13 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 
 void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int pi = lomuto_partition(array, low, high, size);
+	if (low < high)
+	{
+		int pi = lomuto_partition(array, low, high, size);
 
-        quick_sort_recursive(array, low, pi - 1, size);
-        quick_sort_recursive(array, pi + 1, high, size);
-    }
+		quick_sort_recursive(array, low, pi - 1, size);
+		quick_sort_recursive(array, pi + 1, high, size);
+	}
 }
 
 /**
@@ -78,8 +78,8 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 
 void quick_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    quick_sort_recursive(array, 0, size - 1, size);
+	quick_sort_recursive(array, 0, size - 1, size);
 }
