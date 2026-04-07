c3ca16e3b4e076fadd4039fc1da2facda1c1fea4
Mcpe4ce <phil.ghanem@gmail.com>
2026-04-07 13:32:05 +0200

    Implement bubble sort function and update function prototypes in sort.h

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,15 @@
 #include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+    int verificator;
+    int temp;
+
+    for (verificator = 0; verificator < size; verificator++)
+    {
+        if (verificator > array[verificator])
+        {
+            
+        }
+    }
+}
