600afa10dca969f017d3af43c7472faedd395f69
Mcpe4ce <phil.ghanem@gmail.com>
2026-04-07 13:55:49 +0200

    Fix bubble sort function logic and update function prototypes in sort.h

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,13 +3,25 @@
 void bubble_sort(int *array, size_t size)
 {
     int verificator;
+    int index;
     int temp;
 
-    for (verificator = 0; verificator < size; verificator++)
+    if (array == NULL || size < 2)
     {
-        if (verificator > array[verificator])
+        return (NULL);
+    }
+
+    for (verificator = 0; verificator < size - 1; verificator++)
+    {
+        for (index = 0; index < size - 1 - verificator; index++)
         {
-            
+            if (array[verificator] > array[verificator + 1])
+            {
+                temp = array[index];
+                array[index] = array[index + 1];
+                array[index + 1] = temp;
+                print_array(array, size);
+            }
         }
     }
 }
